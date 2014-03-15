f1f4582d6b952830cc86fb75e94149884b43e0f0
PinkieSwirl <pinkie.swirl@gmx.de>
2014-03-15 15:52:19 +0100

    changed interfaces

--- CoreEngine/src/de/fatochs/engine/core/entities/Entity2D.java
+++ CoreEngine/src/de/fatochs/engine/core/entities/Entity2D.java
@@ -3,11 +3,22 @@
  */
 package de.fatochs.engine.core.entities;
 
+import com.badlogic.gdx.math.Vector2;
+
 /**
  * @author pinkie.swirl@mailbox.org
  *
  */
 public abstract class Entity2D implements Entity {
+
+	/**
+	 * The 2D position vector.
+	 */
+	public Vector2 position;
+	/**
+	 * The 2D velocity vector.
+	 */
+	public Vector2 velocity;
 
 	/**
 	 * 
